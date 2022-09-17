5b7c8a49a94125ef4a5fe1cb856329686640bb85
victor <victorzugadi@bamatic.com>
2022-09-17 20:42:58 +0000

    testing 20000 batch

--- services/CouchDBPythonService.py
+++ services/CouchDBPythonService.py
@@ -6,25 +6,24 @@ from models.couchdb_request.CouchdbResponse import CouchdbResponse
 
 
 class CouchDBPythonService(CouchdbChangesHttpService):
-    def get_batch(self, request_config: CouchdbRequest, auth: CouchdbAuth) -> CouchdbResponse:
-        url = f"{request_config.protocol}://{auth.username}:{auth.password}@{request_config.domain}"
+    def get_batch(self, options: dict = None) -> CouchdbResponse:
+        url = f"{CouchdbRequest.protocol}://{CouchdbAuth.username}:{CouchdbAuth.password}@{CouchdbRequest.domain}"
         server = Server(url)
-        response = CouchdbResponse()
         try:
             db = server['medic']
             print("authorized")
-            if request_config.since is None:
-                changes = db.changes(include_docs=True, since=0, limit=request_config.limit)
+            if CouchdbRequest.since is None:
+                changes = db.changes(include_docs=True, since=0, limit=CouchdbRequest.limit)
             else:
-                changes = db.changes(include_docs=True, since=request_config.since, limit=request_config.limit)
-            response.last_sequence_number = changes["last_seq"]
-            response.pending = changes["pending"]
-            response.docs = changes["results"]
+                changes = db.changes(include_docs=True, since=CouchdbRequest.since, limit=CouchdbRequest.limit)
+            CouchdbResponse.last_sequence_number = changes["last_seq"]
+            CouchdbResponse.pending = changes["pending"]
+            CouchdbResponse.docs = changes["results"]
         except Unauthorized:
             print("unauthorized")
-            response.pending = -1
-            response.last_sequence_number = None
-            response.docs = []
+            CouchdbResponse.pending = -1
+            CouchdbResponse.last_sequence_number = None
+            CouchdbResponse.docs = []
 
-        return response
+        return CouchdbResponse()
 
